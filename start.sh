9bb8c4b9cbb8d477456b6d3b30ade678d25a7ce8
Tetsuri Moriya <tetsuri.moriya@gmail.com>
2021-09-30 16:30:54 +0900

    small fixes on start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 DOCKER_FILE_PATH=docker/Dockerfile
+DOCKER_IMAGE_NAME=c8ydm-image
+DOCKER_CONTAINER_NAME=c8ydm
 # construct build args from env
 function env_build_arg() {
-    BUILD_ARG_LIST=$(grep -oP '^ARG .*(?==)' docker/Dockerfile | cut -d' ' -f2-)
+    BUILD_ARG_LIST=$(grep -oP '^ARG .*(?==)' "$DOCKER_FILE_PATH" | cut -d' ' -f2-)
     BUILD_ARG_ARG=''
     for BUILD_ARG in $BUILD_ARG_LIST
     do
@@ -14,7 +16,14 @@ function env_build_arg() {
     done
     echo $BUILD_ARG_ARG
 }
-docker build -t dm-image -f "$DOCKER_FILE_PATH" $(env_build_arg) .
+docker build -t $DOCKER_IMAGE_NAME -f "$DOCKER_FILE_PATH" $(env_build_arg) .
+# check interactivity
+INTERACTIVITY_ARG='-d'
+if [ "${INTERACTIVE:-}" = 1 ]
+then
+    INTERACTIVITY_ARG='-it'
+fi
 # load variables starting with "C8YDM"
 docker run --env-file <(env | grep C8YDM) \
-           -d -v /var/run/docker.sock:/var/run/docker.sock dm-image
+           --name $DOCKER_CONTAINER_NAME --rm $INTERACTIVITY_ARG \
+           -v /var/run/docker.sock:/var/run/docker.sock $DOCKER_IMAGE_NAME
